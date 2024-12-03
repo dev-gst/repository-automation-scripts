1b4183bbdfb5dde9b8a44f73e509e7bc059de76b
Gustavo Garcia <gst.garc@gmail.com>
2024-12-03 02:02:33 -0300

    feat: script to create a project folder, init a git repo on it and commit a README.md file

---
+++ sh/create_repository.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+case $# in
+    1)
+        PROJECT_PATH=$(pwd)
+        PROJECT_NAME=$1
+        ;;
+    2)
+        PROJECT_PATH=$1
+        PROJECT_NAME=$2
+        ;;
+    *)
+        echo "Usage: $0 [project_path] <project_name>"
+        exit 1
+        ;;
+esac
+
+PROJECT_PATH=${PROJECT_PATH%/}
+PROJECT_NAME=${PROJECT_NAME%/}
+FULL_PATH=$PROJECT_PATH/$PROJECT_NAME
+
+mkdir -p $FULL_PATH
+if [ $? -ne 0 ]; then
+    echo "Failed to create directory"
+    exit 1
+fi
+
+touch $FULL_PATH/README.md
+if [ $? -ne 0 ]; then
+    echo "Failed to create file"
+    exit 1
+fi
+
+(
+    cd $FULL_PATH
+    echo "projeto $PROJECT_NAME inicializado...." >> ./README.md
+    git init
+    git add $FULL_PATH/README.md
+    git commit -m "first commit - repositório configurado"
+) > /dev/null 2>&1
+
+exit 0
